e19c0576ccc09ca58f3604a66f7fc32c48b86809
lindaoktaviyani <oktaviyani.linda@gmail.com>
2023-06-17 20:43:47 +0800

    template

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -4,6 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Menu extends CI_Controller{
     public function index()
     {
-        $this->load->view('template/menu');
+        $data = array(
+            'content' => 'dashboard/index.php'
+        );
+        $this->load->view('template/menu',  $data);
     }
 }
