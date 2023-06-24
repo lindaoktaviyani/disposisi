f3cdf2dd4068c23b4e7176df3295e418b836d2b3
lindaoktaviyani <oktaviyani.linda@gmail.com>
2023-06-24 21:25:16 +0800

    add login dan fitur  user

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -41,6 +41,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $autoload['packages'] = array();
 
+$autoload['libraries'] = array('database','session','form_validation');
+
+$autoload['driver'] = array();
+
+$autoload['helper'] = array('url','pos');
 /*
 | -------------------------------------------------------------------
 |  Auto-load Libraries
